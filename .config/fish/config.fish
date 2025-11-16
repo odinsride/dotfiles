d7194e10eca3c1ab7b4f171b23727bc3272986d0
Kevin Custer <kevin@kevin-custer.com>
2025-11-16 10:58:31 -0500

    Add aliases to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,6 @@ alias vi='nvim'
 alias vim='nvim'
 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+alias mount-lib='sudo mount -t cifs //saturn.local/library /mnt/library -o credentials=$HOME/.smbcredentials,uid=1000,gid=1000,file_mode=0744,dir_mode=0755'
+alias umount-lib='sudo umount /mnt/library'
