4ea1481432b5813faa4b3926cce1c9fe13f39b33
Kevin Custer <kevin@kevin-custer.com>
2026-04-14 22:48:10 -0400

    Updates for tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,3 +21,10 @@ else if test (uname) = Linux
     source ~/.config/fish/linux.fish
 end
 /home/sitheris/.local/bin/mise activate fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/sitheris/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
